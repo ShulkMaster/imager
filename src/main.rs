ed28d0631c2240c923f11d37456e5065d25d87ac
Yury Rivera <yrivera@applaudostudios.com>
2022-12-10 14:51:32 -0600

    creating image buff from original image

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-use image::ImageBuffer;
+use image;
 
 fn main() {
-    println!("Hello, world!");
+    let img = image::open("images/test.png").unwrap();
+    let mut buff = image::ImageBuffer::new(img.width(), img.height());
+    for x in 0..img.width() {
+        for y in 0..img.height() {
+            if y >= img.height() / 2 {
+                let pixe = image::Rgb([100u8, 100u8, 200u8]);
+                buff.put_pixel(x, y, pixe);
+            } else {
+                let pixe = image::Rgb([50u8, 50u8, 100u8]);
+                buff.put_pixel(x, y, pixe);
+            }
+        }
+    }
+    buff.save("output.png").unwrap();
 }
