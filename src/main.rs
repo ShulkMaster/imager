100173d8b9054a90e6f62caee4df62c612441beb
Yury Rivera <yrivera@applaudostudios.com>
2022-12-10 15:06:51 -0600

    generating luminance map manually

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-use image;
+use image::{self, GenericImageView};
 
 fn main() {
     let img = image::open("images/test.png").unwrap();
-    let mut buff = image::ImageBuffer::new(img.width(), img.height());
+    let mut buff: image::ImageBuffer<image::Luma<u8>, Vec<u8>> = image::ImageBuffer::new(img.width(), img.height());
     for x in 0..img.width() {
         for y in 0..img.height() {
-            if y >= img.height() / 2 {
-                let pixe = image::Rgb([100u8, 100u8, 200u8]);
-                buff.put_pixel(x, y, pixe);
-            } else {
-                let pixe = image::Rgb([50u8, 50u8, 100u8]);
-                buff.put_pixel(x, y, pixe);
-            }
+            let pixel = img.get_pixel(x, y);
+            let r = pixel[0] as u16;
+            let g = pixel[1] as u16;
+            let b = pixel[2] as u16;
+            let sum = r + g + b;
+            let average = (sum / 3) as u8;
+            let new_pix = image::Luma([average]);
+            buff.put_pixel(x, y, new_pix);
         }
     }
     buff.save("output.png").unwrap();
